2e407611cdfd20c74f0af978341995054d1607e7
Feqan00 <ehmedovfeqan18@gmail.com>
2023-11-16 14:27:44 +0400

    finish

--- tech.js
+++ tech.js
@@ -1,9 +1,10 @@
 const rentBtn = document.getElementById('rentBtn')
-const modal = document.querySelector('.modal-dialog')
-
+const modal = document.querySelector('.modal')
 
 rentBtn.addEventListener('click', () => {
+    modal.classList.remove("show")
     
 })
+
 
 
